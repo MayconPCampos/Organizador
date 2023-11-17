f917a9c89ee79fb7b785958b7f16a0e020a47fd3
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 17:38:16 -0300

    Retirada margem dos titulos

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -29,6 +29,10 @@ const GlobalStyles = createGlobalStyle`
         width: 100vw;
         height: 100vh;
     }
+
+    h1, h2, h3, h4, h5, h6{
+        margin: 0;
+    }
 `;
 
 export default GlobalStyles;
