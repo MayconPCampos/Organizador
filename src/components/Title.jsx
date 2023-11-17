02e3f7bbea9737bf444f4eecb64eb34d0354fd58
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 17:07:16 -0300

    Personalizado Title component e stories

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,17 +3,36 @@ import styled from "styled-components";
 import mainTitleFont from "../fonts/McLaren-Regular.ttf";
 
 const Styles = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
+  @font-face {
+    font-family: "McLaren";
+    src: url(${mainTitleFont});
+  }
+
+  font-family: "DMSans";
+  color: #fff;
 
   h1 {
-    @font-face {
-      font-family: "McLaren";
-      src: url(${mainTitleFont});
-    }
-    color: #fff;
     font-family: "McLaren";
-    padding: 24px;
-    margin: 0;
+  }
+
+  h2 {
+    font-weight: 500;
+  }
+
+  h3 {
+    font-weight: 500;
+  }
+
+  h4 {
+    font-weight: 500;
+  }
+
+  h5 {
+    font-weight: 500;
+  }
+
+  h6 {
+    font-weight: 500;
   }
 `;
 
--- src/stories/Title.stories.jsx
+++ src/stories/Title.stories.jsx
@@ -7,5 +7,14 @@ export default {
 };
 
 export const Main = {
-  render: () => <Title></Title>,
+  render: () => (
+    <Title>
+      <h1>Kapple</h1>
+      <h2>Kapple</h2>
+      <h3>Kapple</h3>
+      <h4>Kapple</h4>
+      <h5>Kapple</h5>
+      <h6>Kapple</h6>
+    </Title>
+  ),
 };
