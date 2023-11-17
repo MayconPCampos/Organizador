264a3639e74fa82fafb4a12f869b579f381b8c10
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 19:38:12 -0300

    Alterado display para flex

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 const Container = styled.div`
   padding: 16px;
+  display: flex;
+  gap: 16px;
 `;
 
 const Workspace = ({ children }) => <Container>{children}</Container>;
