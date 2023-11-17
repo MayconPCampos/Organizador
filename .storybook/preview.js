015938135b29934b9f326a4556503baf3dcd731e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 16:05:48 -0300

    Implementado estilo global apra todos os stories

---
+++ .storybook/preview.js
@@ -0,0 +1,26 @@
+import GlobalStyles from "../src/styles/GlobalStyles";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+// decorator para aplicar tema global em todos os stories
+export const decorators = [
+  (StoryFn) => (
+    <>
+      <GlobalStyles />
+      <StoryFn />
+    </>
+  ),
+];
+
+export default preview;
