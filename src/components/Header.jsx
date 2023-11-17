367493a64933219fa04cc1974fb38e8e16d2f282
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 17:21:48 -0300

    Criado Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  width: 100%;
+  height: 64px;
+  padding: 8px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+`;
+
+export default Header;
