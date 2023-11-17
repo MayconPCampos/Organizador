55074ccef117f0f3d2342e98761e0aff66b283ef
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 18:13:19 -0300

    Criado Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: ${(props) => props.background};
+  padding: 16px;
+  border-radius: 5px;
+`;
+
+export default Card;
