15ee065ebf8bce08ecd2b2e4361853e86466d8a4
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 16:57:16 -0300

    Adicionadas fonte principal

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 import backgroundImage from "../assets/bg.avif";
+import titleFontLight from "../fonts/DMSans_18pt-Light.ttf";
+import titleFontMedium from "../fonts/DMSans_18pt-Medium.ttf";
 
 const GlobalStyles = createGlobalStyle`
+
+    @font-face {
+        font-family: "DMSans";
+        src: url(${titleFontLight});
+        font-weight: 300;
+    }
+
+    @font-face {
+        font-family: "DMSans";
+        src: url(${titleFontMedium});
+        font-weight: 500;
+    }
 
     body{
         background: url(${backgroundImage}), rgba(0,0,0,0.2);
