3d8aa69caabe12ddc42d86f29144dd502135ede4
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 18:20:43 -0300

    Personalizado Page stories

--- src/stories/Page.stories.jsx
+++ src/stories/Page.stories.jsx
@@ -1,17 +1,32 @@
 import Workspace from "../components/Workspace";
 import Title from "../components/Title";
+import Header from "../components/Header";
 import Board from "../components/Board";
+import Card from "../components/Card";
 
 export default {
-  title: "Page",
+  title: "Layout/Page",
 };
 
 export const Main = {
   render: () => (
     <>
-      <Title></Title>
+      <Header>
+        <Title>
+          <h1>Kapple</h1>
+        </Title>
+      </Header>
       <Workspace>
-        <Board></Board>
+        <Board>
+          <Title>
+            <h3>✈️ Travel to Paris</h3>
+          </Title>
+          <Card background="#d1f1eb">
+            <Title>
+              <h4>Visit Eifel Tower</h4>
+            </Title>
+          </Card>
+        </Board>
       </Workspace>
     </>
   ),
