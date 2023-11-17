fc0a07e4751fe0740353541c250d1f5379147be3
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 19:35:08 -0300

    Margem adicionada

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Card = styled.div`
   background-color: ${(props) => props.background};
   padding: 16px;
+  margin: 0 2px;
   border-radius: 5px;
 `;
 
