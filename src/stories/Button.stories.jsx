9c8fb555ae01c20a104a50c5203ce1e321b6e5f6
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-17 19:36:11 -0300

    Adicionado border-box ao documento html

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "../components/Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+export const Primary = {
+  render: () => <Button text="+ add new"></Button>,
+};
