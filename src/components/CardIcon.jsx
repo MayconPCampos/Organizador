3c3584adc448efc4b980adc6138226749a074a9f
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-22 15:07:43 -0300

    Criado cardIcon component

---
+++ src/components/CardIcon.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const CardIcon = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 5px;
+
+  img {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+  }
+`;
+
+export default CardIcon;
