5754b71c62260d4c78e21452411a5e589f928e87
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-22 16:50:40 -0300

    Adicionadas cores dos cards do tema

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,5 +1,12 @@
 const theme = {
   colors: {
+    cards: {
+      yellow: "#f1f58f",
+      orange: "#ffa930",
+      pink: "#ff4c78",
+      blue: "#87b8ea",
+      green: "#a9e37c",
+    },
     text: {
       dark: "#333",
       light: "#fff",
