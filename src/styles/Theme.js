6b6c74f4a50071a93048788afd516e947969cfd9
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-22 12:40:59 -0300

    Criado tema

---
+++ src/styles/Theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    text: {
+      dark: "#333",
+      light: "#fff",
+    },
+    background: {
+      light: "#fff",
+    },
+    button: {
+      main: "#bbb",
+    },
+  },
+};
+
+export default theme;
