d478aefe89c3ad5282e7d9ab2a601be90fcefc85
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-07 14:26:09 -0300

    Adicionado grid, height e alterada cor de fundo

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: rgba(0, 0, 0, 0.6);
-  width: 300px;
-  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100%;
+  grid-column: 1 / 3;
+  grid-row: 2 / 5;
 `;
 
 const SideBar = ({ children }) => {
