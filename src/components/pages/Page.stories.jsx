44b34ee288ccd37a279947df2154bd77a027d08a
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-07 14:30:34 -0300

    Personalizada stories da página

--- src/components/pages/Page.stories.jsx
+++ src/components/pages/Page.stories.jsx
@@ -1,10 +1,13 @@
 import Workspace from "../organisms/Workspace";
+import Toolbar from "../organisms/Toolbar";
 import Title from "../atoms/Title";
 import Header from "../molecules/Header";
+import SideBar from "../templates/SideBar";
 import Board from "../molecules/Board";
 import Card from "../organisms/Card";
 import CardIcon from "../atoms/CardIcon";
 import Button from "../atoms/Button";
+import Grid from "../atoms/Grid";
 
 //svg images
 import commentaryIcon from "../../assets/comments-solid.svg";
@@ -18,39 +21,43 @@ export default {
 export const Main = {
   render: () => (
     <>
-      <Header>
-        <Title>
-          <h1>Kapple</h1>
-        </Title>
-      </Header>
-      <Workspace>
-        <Board>
+      <Grid col={12}>
+        <Header>
           <Title>
-            <h3>✈️ Travel to Paris</h3>
+            <h1>Kapple</h1>
           </Title>
-          <Card background={(props) => props.theme.colors.cards.blue}>
+        </Header>
+        <Toolbar />
+        <SideBar />
+        <Workspace>
+          <Board>
             <Title>
-              <h4>Visit Eifel Tower</h4>
+              <h3>✈️ Travel to Paris</h3>
             </Title>
-            <CardIcon>
-              <img src={descriptionIcon} alt="" />
-              <img src={commentaryIcon} alt="" />
-            </CardIcon>
-          </Card>
-          <Card background="#ff4c78">
-            <img src={cardImage} alt="" />
-            <Title>
-              <h4>Card title</h4>
-            </Title>
-            <CardIcon>
-              <img src={descriptionIcon} alt="" />
-              <img src={commentaryIcon} alt="" />
-            </CardIcon>
-          </Card>
-          <Button text="+ Add new card" />
-        </Board>
-        <Button text="+ Add new Board"></Button>
-      </Workspace>
+            <Card background={(props) => props.theme.colors.cards.blue}>
+              <Title>
+                <h4>Visit Eifel Tower</h4>
+              </Title>
+              <CardIcon>
+                <img src={descriptionIcon} alt="" />
+                <img src={commentaryIcon} alt="" />
+              </CardIcon>
+            </Card>
+            <Card background="#ff4c78">
+              <img src={cardImage} alt="" />
+              <Title>
+                <h4>Card title</h4>
+              </Title>
+              <CardIcon>
+                <img src={descriptionIcon} alt="" />
+                <img src={commentaryIcon} alt="" />
+              </CardIcon>
+            </Card>
+            <Button text="+ Add new card" />
+          </Board>
+          <Button text="+ Add new Board"></Button>
+        </Workspace>
+      </Grid>
     </>
   ),
 };
