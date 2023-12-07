d7ee3b6b9facd8df37c05584d1efc66ed784a32a
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-07 13:38:59 -0300

    Modificado nome de props

--- src/components/atoms/Grid.js
+++ src/components/atoms/Grid.js
@@ -3,33 +3,33 @@ import propTypes from "prop-types";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(${(props) => props.columns}, 1fr);
+  grid-template-columns: repeat(${(props) => props.col}, 1fr);
   gap: 16px;
 `;
 
 Grid.defaultProps = {
-  columns: 4,
+  col: 4,
 };
 
 Grid.propTypes = {
-  columns: propTypes.number,
+  col: propTypes.number,
 };
 
 export const Item = styled.div`
-  grid-column: ${(props) => props.columnStart} / ${(props) => props.columnEnd};
+  grid-column: ${(props) => props.colStart} / ${(props) => props.colEnd};
   grid-row: ${(props) => props.rowStart} / ${(props) => props.rowEnd};
 `;
 
 Item.defaultProps = {
-  columnStart: 1,
-  columnEnd: -1,
+  colStart: 1,
+  colEnd: -1,
   rowStart: 1,
   rowEnd: -1,
 };
 
 Item.propTypes = {
-  columnStart: propTypes.number,
-  columnEnd: propTypes.number,
+  colStart: propTypes.number,
+  colEnd: propTypes.number,
   rowStart: propTypes.number,
   rowEnd: propTypes.number,
 };
