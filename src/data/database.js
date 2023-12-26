3a072a12930757f41d6d83e2a98adc7f6bfa0dcf
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-26 12:42:40 -0300

    Criada lista de dicionario com boards e cards

---
+++ src/data/database.js
@@ -0,0 +1,22 @@
+const boards = [
+  {
+    key: 1,
+    title: "✒️Design & painting",
+    cards: [
+      {
+        key: 1,
+        title: "painting",
+        background: "#ff4c78",
+        image: "./assets/img01.jpg",
+      },
+      {
+        key: 2,
+        title: "drawing",
+        background: "#a9e37c",
+        image: "",
+      },
+    ],
+  },
+];
+
+export default boards;
