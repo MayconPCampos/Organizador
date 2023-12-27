4724284a975afb5a4d8da2d08ca599a6bf2ce88e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-27 11:47:15 -0300

    Personalizado componente

--- src/components/organisms/CardCreation.jsx
+++ src/components/organisms/CardCreation.jsx
@@ -10,10 +10,66 @@ const Styled = styled.div`
   margin: 2% 20%;
   border: 1px solid rgba(0, 0, 0, 0.5);
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+
+  label {
+    display: block;
+    font-size: 1.2rem;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .description {
+    width: 100%;
+    height: 40px;
+    resize: none;
+  }
+
+  .children {
+    position: absolute;
+    bottom: 10px;
+    right: 20px;
+  }
+
+  .commentary {
+    width: 100%;
+    height: 80px;
+    resize: none;
+  }
 `;
 
 const CardCreation = ({ children }) => {
-  return <Styled>{children}</Styled>;
+  return (
+    <Styled>
+      <form action="">
+        <label>Marcador</label>
+        <input type="color" />
+        <label>Descrição</label>
+        <textarea
+          autofocus
+          className="description"
+          name=""
+          id=""
+          cols="1"
+          rows="1"
+        ></textarea>
+        <label>Adicionar comentário</label>
+        <textarea
+          autofocus
+          className="commentary"
+          name=""
+          id=""
+          cols="1"
+          rows="1"
+        ></textarea>
+        <label htmlFor="">Carregar imagem</label>
+        <input type="file" />
+      </form>
+      <div className="children">{children}</div>
+    </Styled>
+  );
 };
 
 CardCreation.defaultProps = {
