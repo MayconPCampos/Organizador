d79d98dfb99387f23a3d250de714fdef8414bf8b
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-27 10:42:25 -0300

    Adicionada novas entradas na base de dados dos boards e cards

--- src/data/database.js
+++ src/data/database.js
@@ -1,17 +1,101 @@
 const boards = [
   {
-    key: 1,
+    key: "a",
     title: "✒️Design & painting",
     cards: [
       {
-        key: 1,
-        title: "painting",
-        background: "#ff4c78",
+        key: "a1",
+        title: "Nullam bibendum ante sed magna vulputate",
+        background: "#fff",
         image: "./assets/img01.jpg",
       },
       {
-        key: 2,
-        title: "drawing",
+        key: "a2",
+        title: "Neque porro quisquam est",
+        background: "#a9e37c",
+        image: "",
+      },
+    ],
+  },
+  {
+    key: "b",
+    title: "📌Decoration",
+    cards: [
+      {
+        key: "b1",
+        title: "Praesent scelerisque iaculis",
+        background: "#ff4c78",
+        image: "",
+      },
+      {
+        key: "b2",
+        title: "Nunc sodales dictum",
+        background: "#ffa930",
+        image: "",
+      },
+      {
+        key: "b3",
+        title: "Nunc sodales dictum",
+        background: "#ffa930",
+        image: "./assets/pic2.avif",
+      },
+      {
+        key: "b4",
+        title: "Neque porro quisquam est qui dolorem ipsum",
+        background: "#87b8ea",
+        image: "",
+      },
+      {
+        key: "b5",
+        title: "Nullam bibendum ante sed magna vulputate",
+        background: "#a9e37c",
+        image: "./assets/pic5.avif",
+      },
+      {
+        key: "b6",
+        title: "Praesent scelerisque",
+        background: "#bd87d6",
+        image: "",
+      },
+    ],
+  },
+  {
+    key: "c",
+    title: "🍝Food & gastronomy",
+    cards: [
+      {
+        key: "c1",
+        title: "Nullam bibendum ante sed magna vulputate",
+        background: "#a9e37c",
+        image: "./assets/pic4.avif",
+      },
+      {
+        key: "c2",
+        title: "Neque porro quisquam",
+        background: "#bd87d6",
+        image: "",
+      },
+    ],
+  },
+  {
+    key: "d",
+    title: "✈️Places to visit",
+    cards: [
+      {
+        key: "d1",
+        title: "Nullam bibendum ante sed magna vulputate",
+        background: "#ffa930",
+        image: "./assets/pic3.avif",
+      },
+      {
+        key: "d2",
+        title: "Neque porro quisquam",
+        background: "#bd87d6",
+        image: "",
+      },
+      {
+        key: "d3",
+        title: "Neque porro quisquam est qui dolorem ipsum",
         background: "#a9e37c",
         image: "",
       },
