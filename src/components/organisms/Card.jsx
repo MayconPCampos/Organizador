bb827e45a12964e84396e5b50c4da282eaefa2b5
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-27 11:03:02 -0300

    Alterado atributo css de background color e box-shadow

--- src/components/organisms/Card.jsx
+++ src/components/organisms/Card.jsx
@@ -2,12 +2,11 @@ import styled from "styled-components";
 import propTypes from "prop-types";
 
 const Card = styled.div`
-  background-color: ${(props) => props.background};
-  /* background-color: #fff; */
+  background-color: #fff;
   padding: 16px;
   margin: 0 2px;
   border-radius: 5px;
-  box-shadow: 1px 2px #dadada;
+  box-shadow: 1px 1px #dadada;
   display: flex;
   flex-direction: column;
   max-width: 300px;
