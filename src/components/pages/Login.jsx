ddcaa54b349af43086da6fb38716beeffd780f49
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-05 14:57:23 -0300

    Criado component login e respectivo stories

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Workspace from "../organisms/Workspace";
+import Title from "../atoms/Title";
+
+const Login = () => {
+  return (
+    <>
+      <Workspace>
+        <Title>
+          <h1>Kapple</h1>
+        </Title>
+      </Workspace>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/pages/Login.stories.jsx
@@ -0,0 +1,10 @@
+import Login from "./Login";
+
+export default {
+  title: "Components/Pages/Login",
+  component: Login,
+};
+
+export const Usage = {
+  render: () => <Login></Login>,
+};
